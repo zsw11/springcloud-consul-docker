c3eddf0f63d234276a84cfe24c0653b52bb2b848
zsw <546232194@qq.com>
2021-03-17 18:29:33 +0800

    编写 部署docker 脚本文件

---
+++ dashboard/src/main/java/com/zsw/dashboard/DashboardApplication.java
@@ -0,0 +1,18 @@
+package com.zsw.dashboard;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableHystrixDashboard//监测仪表盘
+public class DashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DashboardApplication.class, args);
+    }
+
+}
