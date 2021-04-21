445c82515a709f4f4a253aa1ba2151ca9e6408e3
zsw <546232194@qq.com>
2021-04-21 18:07:28 +0800

    整合 ElasticSearch

--- provider/src/main/java/com/zsw/provider/config/ElasticSearchClientConfig.java
+++ provider/src/main/java/com/zsw/provider/config/ElasticSearchClientConfig.java
@@ -2,9 +2,7 @@ package com.zsw.provider.config;
 
 import org.apache.http.HttpHost;
 import org.elasticsearch.client.RestClient;
-import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
-import org.springframework.boot.autoconfigure.elasticsearch.rest.RestClientAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
